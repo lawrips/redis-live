9fd4dc49c7f98a8a0114045e74393a1c7c81db02
lawrips <lawrence@ripsher.com>
2016-12-10 19:56:46 -0800

    minor changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var debug = require('debug')('redis-live');
-var app = require('./lib/app');
+'use strict';
+
+const debug = require('debug')('redis-live'),
+    app = require('./lib/app');
 
 process.on('uncaughtException', function (err) {
     console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
